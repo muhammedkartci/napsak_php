98c94beff4baeb71527b53c3c3f6414dd8b7af06
muhammedkartci <muhammed.kartci@gmail.com>
2023-02-15 14:11:47 +0300

    site oluşturmaya başlandı

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="napsak-logo.svg" type="image/x-icon" />
+    <title>Napsak | AMEAN Danışmanlık</title>
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <div class="logo">
+                <img class="img" src="napsak-logo.svg" alt="">
+            </div>
+            <div class="text"></div>
+            <div class="napsak-button">
+                <img class="button" src="napsak-button.svg" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
